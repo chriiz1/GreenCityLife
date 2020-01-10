3967a32bc50cb9f1fc11ffd064347afdec6dd6dc
christoph proß <christoph.pross@gmx.at>
2020-01-10 09:01:52 +0100

    Add approaches for Authentication, Examples of Database-UI Interaction

--- app/src/main/java/com/example/greencitylife/classes.kt
+++ app/src/main/java/com/example/greencitylife/classes.kt
@@ -1,24 +1,34 @@
 package com.example.greencitylife
 
+import java.util.*
+
+
 data class Garden(
     var name: String = "",
     var address: String = ""
 )
 
 data class User(
+    var UID: String = "",
     var name: String = "",
+    var role: String = "",
+    var gardenId: String = ""
+)
+//save()
+
+data class Entry(
+    var name: String = "",
+    var category: String = "",
+    var type: String = "",
+    var additionalText: String = "",
+    var userId: String = "",
     var gardenId: String = ""
 )
 
-data class Entry(
+data class Comment(
+    var text: String = "",
+    var date: Date = Calendar.getInstance().time,
     var userId: String = "",
-    var gardenId: String = "",
-    var gardeningStuffId: String = "",
-    var type: String = "",
-    var additionalText: String = ""
+    var entryId: String = ""
 )
 
-data class GardeningStuff(
-    var name: String = "",
-    var category: String = ""
-)
