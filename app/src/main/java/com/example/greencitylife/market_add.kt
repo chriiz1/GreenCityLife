4ef769766d29352ec028b65e49a5fcc6c47b4730
David Röbl <david.roebl@yahoo.com>
2020-02-08 09:54:18 +0100

    create entry formula

--- app/src/main/java/com/example/greencitylife/market_add.kt
+++ app/src/main/java/com/example/greencitylife/market_add.kt
@@ -1,14 +1,21 @@
 package com.example.greencitylife
 
-
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_market_add.*
 
 /**
  * A simple [Fragment] subclass.
@@ -21,16 +28,98 @@ class market_add : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_market_add, container, false)
+        // get context
+        val thiscontext = container!!.getContext()
 
+        // set color and text color of market button
         val market_button = view.findViewById<Button>(R.id.market_button)
         market_button.setBackgroundColor(Color.LTGRAY)
         market_button.setTextColor(Color.WHITE)
 
+        // set color and text color of add button
         val add_button = view.findViewById<ImageButton>(R.id.offer_button)
         add_button.setBackgroundColor(resources.getColor(R.color.button_market_clicked))
 
+        // if load picture button is pressed user is asked to give permission to go to gallery
+        val img_pick_btn = view.findViewById<Button>(R.id.img_pick_btn)
+
+
+        img_pick_btn.setOnClickListener {
+            //check runtime permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(thiscontext, Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED){
+                    //permission denied
+                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+                    //show popup to request runtime permission
+                    requestPermissions(permissions, PERMISSION_CODE);
+                }
+                else{
+                    //permission already granted
+                    pickImageFromGallery();
+                }
+            }
+            else{
+                //system OS is < Marshmallow
+                pickImageFromGallery();
+            }
+        }
+
+        // create toast when submit_entry_button is pressed
+        val submit_button = view.findViewById<Button>(R.id.submit_button)
+        submit_button.setOnClickListener{
+            Toast.makeText(thiscontext, "Your entry has been created", Toast.LENGTH_SHORT).show();
+        }
+
         return view
+
     }
+
+
+    private fun pickImageFromGallery() {
+        //Intent to pick image
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
+
+
+    companion object {
+        //image pick code
+        private val IMAGE_PICK_CODE = 1000;
+        //Permission code
+        private val PERMISSION_CODE = 1001;
+    }
+
+
+    //handle requested permission result
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                    //permission from popup granted
+                    pickImageFromGallery()
+                }
+                else{
+                    //permission from popup denied
+                    Toast.makeText(activity,"Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
+    //handle result of picked image
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            entry_image.setImageURI(data?.data)
+        }
+    }
+
+
+
 
 
 }
